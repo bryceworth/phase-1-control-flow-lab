224edc07566c1d15c1100970c1fb58257a3edb24
bryceworth <bryceaworth@gmail.com>
2022-03-22 11:25:11 -0700

    commit tree e011b42803abac2d96c9da66ec40e62a4427c1f7

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+ let result 
+ if (ride <= 400 ) { result = `This one is on me!`}
+ if (ride >2000 ) {result = 'I will gladly take your thirty bucks.'}
+ if (ride >2500 ) { result = 'No can do.'}
+ return result
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+
+function ternaryCheckCity(city){
+  console.log (city)
+return (city === `NYC` ? `Ok, sounds good.` : `No go.`)
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let result 
+  if (tip === `generous`) {result = 'Thank you so much.'}
+  if (tip === `not as generous`) {result = `Thank you.`}
+  if (tip === `thanks for everything`) {result = `Bye.`}
+  return result
 }
